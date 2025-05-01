566991da264954a5567d18a0c8e170b4232b75f6
umarabid123 <umarabid709@gmail.com>
2025-05-01 14:01:57 +0500

    deploy

--- screens/HomeScreens/HomeScreen.tsx
+++ screens/HomeScreens/HomeScreen.tsx
@@ -8,6 +8,8 @@ import { wp } from '../../contexts/constant';
 const HomeScreen = () => {
   return (
     <View style={[globalStyle.container, {justifyContent: 'flex-start'}]}>
+
+      {/* header  */}
       <View style={{width:wp(90), flexDirection:'row', justifyContent:'space-between'}}>
         {/* location  */}
         <View
@@ -36,10 +38,17 @@ const HomeScreen = () => {
        <View style={{ justifyContent:'center', alignItems:'center',borderRadius:50, borderWidth:1, borderColor:Colors.primary, width:50, height:50}}>
           <Image source={require('../../assets/images/ion_notifications-outline.png')} />
         </View>
-        <Image source={require('../../assets/images/profile-img.jpg')} style={{width:44, height:44, borderRadius:50}} />
+        <View style={{ borderWidth:1.2, borderColor:"#DFDFDF",borderRadius:50,padding:1}}>
+        <Image source={require('../../assets/images/profile-img.jpg')} style={{width:44, height:44, borderRadius:50,}} />
+        </View>
        </View>
 
 
+      </View>
+
+      {/* welcome section  */}
+      <View style={{width:wp(90),marginTop:35}}>
+        <AppText text={ "Hey,"} type='subText' fontSize={25} fontWeight={500} color={Colors.secondary} subTextStyle={{fontWeight:800}} subText={"Jonathan! \nLet's start exploring"} />
       </View>
     </View>
   );
