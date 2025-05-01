0f4a69336ca3b6683203b3954e8c3ce9ef4ef7b9
umarabid123 <umarabid709@gmail.com>
2025-05-01 13:03:39 +0500

    deploy

--- screens/HomeScreens/HomeScreen.tsx
+++ screens/HomeScreens/HomeScreen.tsx
@@ -8,6 +8,7 @@ const HomeScreen = () => {
   return (
     <View style={[globalStyle.container, {justifyContent: 'flex-start'}]}>
       <View>
+        {/* location  */}
         <View
           style={{
             backgroundColor: Colors.white,
@@ -27,6 +28,10 @@ const HomeScreen = () => {
             color={Colors.secondary}
           />
           <Image source={require('../../assets/images/chevron-down.png')} />
+        </View>
+        
+        <View style={{ justifyContent:'center', alignItems:'center',borderRadius:50, borderWidth:1, borderColor:Colors.primary, width:50, height:50}}>
+          <Image source={require('../../assets/images/ion_notifications-outline.png')} />
         </View>
       </View>
     </View>
