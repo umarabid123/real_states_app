b0eda786b57d1e9c049801370b12933b8b34c5f3
umarabid123 <umarabid709@gmail.com>
2025-05-02 18:02:06 +0500

    deploy

--- components/FeatureCard/FeatureCard.tsx
+++ components/FeatureCard/FeatureCard.tsx
@@ -2,11 +2,23 @@ import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {Colors} from '../../contexts/theme';
 import AppButton from '../AppButton/AppButton';
+import AppText from '../AppText/AppText';
 
 const FeatureCard = () => {
   return (
-    <View style={{backgroundColor:Colors.softGray, width:268, paddingHorizontal:8}}>
-      <Image source={require('../../assets/images/feature-card.png')} style={{width:130, height:140}} />
+    <View
+      style={{
+        backgroundColor: Colors.softGray,
+        width: 268,
+        paddingHorizontal: 8,
+      }}>
+
+        {/* image section  */}
+      <View>
+      <Image
+        source={require('../../assets/images/feature-card.png')}
+        style={{width: 130, height: 140}}
+      />
       <View
         style={{
           backgroundColor: Colors.primary,
@@ -18,8 +30,27 @@ const FeatureCard = () => {
         }}>
         <Image source={require('../../assets/images/heart-white.png')} />
       </View>
-      <AppButton text='Apartment' containerStyle={{width:63, paddingVertical:9, backgroundColor:Colors.lightBlue}} />
+      <AppButton
+        text="Apartment"
+        containerStyle={{
+          width: 63,
+          paddingVertical: 9,
+          backgroundColor: Colors.lightBlue,
+        }}
+      />
+      </View>
 
+      {/* text section  */}
+      <View>
+        <AppText text={'Sky Dandelions \nApartment'} fontSize={12} fontWeight={700} color={Colors.secondary} />
+
+        {/* rate section  */}
+
+        <View style={{flexDirection:'row', gap:2, alignItems:'center'}}>
+            <Image source={require('../../assets/images/star.png')} />
+            <AppText text={'4.9'} fontSize={8} fontWeight={700} color={Colors.lightBlue} />
+        </View>
+      </View>
     </View>
   );
 };
