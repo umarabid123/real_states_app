7dcfe91abe796de7936b8114dd52921c6700ea28
umarabid123 <umarabid709@gmail.com>
2025-05-02 17:32:07 +0500

    deploy

--- screens/HomeScreens/HomeScreen.tsx
+++ screens/HomeScreens/HomeScreen.tsx
@@ -142,19 +142,111 @@ const HomeScreen = () => {
           width: 270,
           height: 180,
           borderRadius: 30,
+          borderBottomStartRadius:0,
           overflow: 'hidden',
           backgroundColor: 'red', // for test
         }}>
         <ImageBackground
           source={require('../../assets/images/bg-img.png')}
           resizeMode="cover"
-          style={{width: '100%', height: '100%', justifyContent: 'center'}}>
-          <View style={{paddingLeft: 20}}>
+          style={{width: '100%', height: '100%',justifyContent: 'center'}}>
+          <View style={{paddingLeft: 20,paddingBottom:20}}>
             <AppText
-              text={'Halloween Sale!'}
+              text={'Halloween \nSale!'}
               fontSize={18}
               fontWeight={700}
               color={Colors.white}
+              style={{letterSpacing: 1}}
+            />
+            <AppText
+              text={'All discount up to 60%'}
+              fontSize={10}
+              color={Colors.white}
+            />
+          </View>
+          <AppButton
+            type="icon"
+            source={require('../../assets/images/right-arrow-icon.png')}
+            containerStyle={{
+              width: 93,
+              borderRadius: 0,
+              borderTopEndRadius: 20,
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              backgroundColor: Colors.secondary,
+              justifyContent: 'center',
+              padding: 0,
+              alignItem: 'center',
+            }}
+          />
+        </ImageBackground>
+      </View>
+      <View
+        style={{
+          width: 270,
+          height: 180,
+          borderRadius: 30,
+          borderBottomStartRadius:0,
+          overflow: 'hidden',
+          backgroundColor: 'red', // for test
+        }}>
+        <ImageBackground
+          source={require('../../assets/images/bg-img.png')}
+          resizeMode="cover"
+          style={{width: '100%', height: '100%',justifyContent: 'center'}}>
+          <View style={{paddingLeft: 20,paddingBottom:20}}>
+            <AppText
+              text={'Halloween \nSale!'}
+              fontSize={18}
+              fontWeight={700}
+              color={Colors.white}
+              style={{letterSpacing: 1}}
+            />
+            <AppText
+              text={'All discount up to 60%'}
+              fontSize={10}
+              color={Colors.white}
+            />
+          </View>
+          <AppButton
+            type="icon"
+            source={require('../../assets/images/right-arrow-icon.png')}
+            containerStyle={{
+              width: 93,
+              borderRadius: 0,
+              borderTopEndRadius: 20,
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              backgroundColor: Colors.secondary,
+              justifyContent: 'center',
+              padding: 0,
+              alignItem: 'center',
+            }}
+          />
+        </ImageBackground>
+      </View>
+      <View
+        style={{
+          width: 270,
+          height: 180,
+          borderRadius: 30,
+          borderBottomStartRadius:0,
+          overflow: 'hidden',
+          backgroundColor: 'red', // for test
+        }}>
+        <ImageBackground
+          source={require('../../assets/images/bg-img.png')}
+          resizeMode="cover"
+          style={{width: '100%', height: '100%',justifyContent: 'center'}}>
+          <View style={{paddingLeft: 20,paddingBottom:20}}>
+            <AppText
+              text={'Halloween \nSale!'}
+              fontSize={18}
+              fontWeight={700}
+              color={Colors.white}
+              style={{letterSpacing: 1}}
             />
             <AppText
               text={'All discount up to 60%'}
